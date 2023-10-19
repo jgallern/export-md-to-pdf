74f4210fa9201d1ad83bde5ef3429f9de3e37db6
jgallern <121575424+jgallern@users.noreply.github.com>
2023-10-19 17:18:23 +0200

    Update export.sh

--- export.sh
+++ export.sh
@@ -6,13 +6,13 @@ if ! command -v pandoc &> /dev/null; then
     exit 1
 fi
 
-# Vérifier si Zathura est installé
+# Vérifie si Zathura est installé
 if ! command -v zathura &> /dev/null; then
     echo "Zathura is not installed yet. Please install it before running this script"
     exit 1
 fi
 
-# Vérifier si un argument (fichier Markdown) a été fourni
+# Vérifie si un argument (fichier Markdown) a été fourni
 if [ $# -ne 1 ]; then
     echo "Utilisation : $0 fichier.md"
     exit 1
@@ -21,6 +21,6 @@ fi
 input_file="$1"
 output_file="${input_file%.md}.pdf"
 
-# Convertir le fichier Markdown en PDF avec Pandoc
+# Convertit le fichier Markdown en PDF avec Pandoc et ouvre le pdf dans zathura
 pandoc "$input_file" -o "$output_file" && zathura "$output_file"
 
