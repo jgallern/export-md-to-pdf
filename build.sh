3b1156a0f078c32908569961dae4abe3c7d90bba
jgallern <121575424+jgallern@users.noreply.github.com>
2023-10-19 18:04:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Vérifie si Pandoc est installé
-if ! command -v pandoc &> /dev/null; then
+if ![command -v pandoc &> /dev/null]; then
     echo "Pandoc is not installed yet. Please install it before running this script"
     exit 1
 fi
 
 # Vérifie si Zathura est installé
-if ! command -v zathura &> /dev/null; then
+if ![command -v zathura &> /dev/null]; then
     echo "Zathura is not installed yet. Please install it before running this script"
     exit 1
 fi
