0acc43908b299a9f337ecc23a91eef39433a6015
jgallern <121575424+jgallern@users.noreply.github.com>
2023-10-19 17:19:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
+#!/bin/sh
+
+# Vérifie si Pandoc est installé
+if ! command -v pandoc &> /dev/null; then
+    echo "Pandoc is not installed yet. Please install it before running this script"
+    exit 1
+fi
+
+# Vérifie si Zathura est installé
+if ! command -v zathura &> /dev/null; then
+    echo "Zathura is not installed yet. Please install it before running this script"
+    exit 1
+fi
+
 sudo cp ./export.sh /usr/bin/export.sh
 sudo chmod +x /usr/bin/export.sh
