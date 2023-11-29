b39597940a1dad1c538cdd1d58a567e5db8af96d
jgallern <121575424+jgallern@users.noreply.github.com>
2023-11-29 13:21:18 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 
 # Vérifie si Pandoc est installé
-if ! command -v pandoc &> /dev/null; then
+if !command -v pandoc &> /dev/null; then
     echo "Pandoc is not installed yet. Please install it before running this script"
     exit 1
 fi
 
 # Vérifie si Zathura est installé
-if ! command -v zathura &> /dev/null; then
+if !command -v zathura &> /dev/null; then
     echo "Zathura is not installed yet. Please install it before running this script"
     exit 1
 fi
 
-# Vérifie si un argument (fichier Markdown) a été fourni
-if [ $# -ne 1 ]; then
-    echo "Utilisation : $0 fichier.md"
-    exit 1
-fi
-
-input_file="$1"
-output_file="${input_file%.md}.pdf"
-
-# Convertit le fichier Markdown en PDF avec Pandoc et ouvre le pdf dans zathura
-pandoc "$input_file" \
--o "$output_file" \
---toc -V lang=fr \
--F mermaid-filter \
---template eisvogel \
---highlight-style tango \
---listings \
---number-sections \
---variable colorlinks=true \
-&& zathura "$output_file" &
-
-
-
+sudo cp ./export.sh /usr/bin/export.sh
+sudo chmod +x /usr/bin/export.sh
