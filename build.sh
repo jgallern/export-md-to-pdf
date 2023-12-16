16e668d4203a14dc28dc4d51fed8bc8b0aaf0b65
jgallern <121575424+jgallern@users.noreply.github.com>
2023-12-16 18:54:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,5 @@ if !command -v pandoc &> /dev/null; then
     exit 1
 fi
 
-# Vérifie si Zathura est installé
-if !command -v zathura &> /dev/null; then
-    echo "Zathura is not installed yet. Please install it before running this script"
-    exit 1
-fi
-
 sudo cp ./export.sh /usr/bin/export.sh
 sudo chmod +x /usr/bin/export.sh
